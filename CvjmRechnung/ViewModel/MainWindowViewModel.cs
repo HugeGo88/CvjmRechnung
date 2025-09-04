d2c76f6606a15d87066aa0d1cd5373fe73964c39
Hugo <hugo.tausch@gmail.com>
2025-09-04 21:33:37 +0200

    Display result of rendering

--- CvjmRechnung/ViewModel/MainWindowViewModel.cs
+++ CvjmRechnung/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Web.WebView2.WinForms;
 using SimpleHtmlToPdf;
 using SimpleHtmlToPdf.Interfaces;
 using SimpleHtmlToPdf.Settings;
@@ -22,6 +23,9 @@ namespace CvjmRechnung.ViewModel
         };
 
         [ObservableProperty]
+        WebView2 webView = new WebView2();
+
+        [ObservableProperty]
         string orderNumber = "";
 
         [ObservableProperty]
@@ -38,6 +42,9 @@ namespace CvjmRechnung.ViewModel
 
         [ObservableProperty]
         DateTime? date = DateTime.Now;
+
+        [ObservableProperty]
+        string pdfPath = "";
 
         [ObservableProperty]
         bool isMember;
@@ -73,6 +80,7 @@ namespace CvjmRechnung.ViewModel
         [RelayCommand]
         async Task GeneratePdf()
         {
+            PdfPath = $"{Directory.GetCurrentDirectory()}\\test.pdf";
             var Converter = App.Current.Services.GetService<IConverter>();
             string content = GetHtmlCodeForInvoice();
             var doc = new HtmlToPdfDocument()
@@ -98,24 +106,31 @@ namespace CvjmRechnung.ViewModel
             };
 
             var pdf = Converter.Convert(doc);
-            File.WriteAllBytes("test.pdf", pdf);
-
+            File.WriteAllBytes($"{Directory.GetCurrentDirectory()}\\Rechnung_{OrderNumber}.pdf", pdf);
+            PdfPath = $"{Directory.GetCurrentDirectory()}\\Rechnung_{OrderNumber}.pdf";
         }
 
         private string GetHtmlCodeForInvoice()
         {
             string content = File.ReadAllText("resources/index.html");
             content = content.Replace("{ADDRESS_FIELD}", File.ReadAllText("resources/addressField.html"));
+            content = content.Replace("{ADDRESS_COMPANY}", "");
             content = content.Replace("{CLUB_NAME}", "CVJM Walheim");
             content = content.Replace("{CLUB_STREET}", "Auf der Burg 6");
             content = content.Replace("{CLUB_CITY}", "Walheim");
-            content = content.Replace("{CLUB_PHONE}", "+49 175 2000509");
+            content = content.Replace("{CLUB_PHONE}", "+49 (0)175 2000509");
             content = content.Replace("{CLUB_HOMEPAGE}", "cvjm-walheim.de");
+            content = content.Replace("{CLUB_EMAIL}", "info@cvjm-walheim.de");
+            content = content.Replace("{CLUB_IBAN}", "DE10 6049 1430 0390 5390 07");
+            content = content.Replace("{CLUB_BIC}", "GENODES1VBB");
+            content = content.Replace("{CLUB_BANK}", "VR-Bank Ludwigsburg eG");
+            content = content.Replace("{CLUB_BOARD1}", "Hugo Tausch");
+            content = content.Replace("{CLUB_BOARD2}", "Stephanie Alber");
+            content = content.Replace("{ORDER_NUMBER}", this.OrderNumber);
             content = content.Replace("{ADDRESS_NAME}", this.FirstAndLastName);
             content = content.Replace("{ADDRESS_STREET}", this.StreetAndNumber);
-            content = content.Replace("{ADDRESS_STREET}", this.StreetAndNumber);
             content = content.Replace("{ADDRESS_CITY}", this.PostalCodeAndCity);
-            content = content.Replace("{DATE}", this.Date.HasValue ? this.Date.Value.ToString("Walheim, dd MMMM yyyy") : "");
+            content = content.Replace("{DATE}", this.Date.HasValue ? this.Date.Value.ToString("dd MMMM yyyy") : "");
             File.WriteAllText("test.html", content);
             return content;
         }
