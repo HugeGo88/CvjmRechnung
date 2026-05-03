51153b53cb4cd950379f777a5bb644e562f02ce9
Hugo <hugo.tausch@gmail.com>
2026-05-03 20:58:17 +0200

    list money

--- CvjmRechnung/Model/Invoice.cs
+++ CvjmRechnung/Model/Invoice.cs
@@ -2,6 +2,7 @@
 using CvjmRechnung.ViewModel;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace CvjmRechnung.Model
@@ -40,6 +41,9 @@ namespace CvjmRechnung.Model
 
         public Guid InvoiceId { get => _invoiceId; set => _invoiceId = value; }
 
+        [XmlIgnore]
+        public double TotalAmount => InvoiceRows.Sum(x => x.TotalPrice);
+
         [ObservableProperty]
         string orderNumber = "";
 
@@ -71,12 +75,55 @@ namespace CvjmRechnung.Model
         ObservableCollection<InvoiceRow> invoiceRows = new();
         private Guid _invoiceId;
 
-        private void InvoiceRows_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        partial void OnInvoiceRowsChanged(ObservableCollection<InvoiceRow> value)
         {
-            OnInvoiceRowsChanged();
+            if (value is null)
+            {
+                return;
+            }
+
+            value.CollectionChanged += InvoiceRows_CollectionChanged;
+            foreach (var row in value)
+            {
+                row.PropertyChanged += InvoiceRow_PropertyChanged;
+            }
+
+            OnPropertyChanged(nameof(TotalAmount));
         }
 
-        private void OnInvoiceRowsChanged()
+        private void InvoiceRows_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems is not null)
+            {
+                foreach (InvoiceRow row in e.NewItems)
+                {
+                    row.PropertyChanged += InvoiceRow_PropertyChanged;
+                }
+            }
+
+            if (e.OldItems is not null)
+            {
+                foreach (InvoiceRow row in e.OldItems)
+                {
+                    row.PropertyChanged -= InvoiceRow_PropertyChanged;
+                }
+            }
+
+            UpdateInvoiceRows();
+            OnPropertyChanged(nameof(TotalAmount));
+        }
+
+        private void InvoiceRow_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(InvoiceRow.Quantity) ||
+                e.PropertyName == nameof(InvoiceRow.UnitPrice) ||
+                e.PropertyName == nameof(InvoiceRow.TotalPrice))
+            {
+                OnPropertyChanged(nameof(TotalAmount));
+            }
+        }
+
+        private void UpdateInvoiceRows()
         {
             for (int i = 0; i < InvoiceRows.Count; i++)
             {
